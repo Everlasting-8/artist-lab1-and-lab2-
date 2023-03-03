51c7ab58d2ced2e29778dcd06242e2f785db8bad
Everlasting-8 <100763058+Everlasting-8@users.noreply.github.com>
2023-03-03 17:46:37 +0800

    Add files via upload

---
+++ Modified/labatwo/authentication-and-authorization-service/src/test/java/com/laba2/authenticationandauthorizationservice/AuthenticationAndAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laba2.authenticationandauthorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
